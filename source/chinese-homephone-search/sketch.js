6786eeb2851cefab9bec00246df2f8f2128c6b45
Qianqian Ye <qianqian.ye44@gmail.com>
2020-10-12 21:58:52 -0700

    added homophone search folder

---
+++ source/chinese-homephone-search/sketch.js
@@ -0,0 +1,58 @@
+var input;
+var homephoneData;
+var foundHomephone=[];
+
+function preload(){
+	JSONArray = loadJSON("./sample.json");
+}
+
+function setup(){
+	homephoneData= JSONArray["dataset"];
+}
+
+function buildFoundDivs(filterItem, inList){
+	
+	const keys = Object.keys(inList)
+	var destDiv = select("#searcResultBox")
+
+	for (var key of keys) {
+		if(key != 'spell'){
+			var valN = inList[key];
+			if(valN != filterItem){
+				var newDiv = createDiv(valN);
+				console.log("add newdiv "+ valN)
+				newDiv.class('searchResult');
+				destDiv.child(newDiv);
+			}	
+		}
+	}
+}
+
+function removeFoundElements(){
+	var pdivs = selectAll('.searchResult');
+	for (var i = 0; i < pdivs.length; i++) {
+		pdivs[i].remove();
+	}
+	
+}
+
+ function searchHomophone(){
+	input = document.getElementById('type').value;
+	console.log(input);
+	for (var i = homephoneData.length - 1; i >= 0; i--) {
+		const keys = Object.keys(homephoneData[i])
+		// console.log(keys)
+		for (const key of keys) {
+			var valN = homephoneData[i][key]
+			if( input == valN){
+				console.log('MATCH FOUND')
+				removeFoundElements()
+			 	buildFoundDivs(input,homephoneData[i]);
+			 	return
+			}
+		}
+	}
+ }
+
+
+
